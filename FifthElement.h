9d0f888a5de03be269beca9c1e7d47319a1f0a6d
Rick <RTSmith801@gmail.com>
2022-12-13 19:46:29 -0700

    initial commit

---
+++ FifthElement.h
@@ -0,0 +1,21 @@
+/**
+ * @file: LinkedList.h
+ * @assignment_name: 03: Try it out: Fifth Element
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 12/13/2022
+ * @brief: This program will cover review Inheritance and give you practice with handling pointers in Linked Lists.
+ *
+ */
+
+#ifndef INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
+#define INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
+
+
+Type getFifthElement(); //This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
+void insertNewFifthElement(const Type &value); //This method inserts a node containing a value between the existing 4 and 5 nodes so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list.
+void deleteFifthElement(); //This method deletes the 5th node. If there was a 6th node, the 4th node now points to the 6th node. If there was no 6th node, the 4th node becomes the new back node.
+void swapFourthAndFifthElement(); //This method rearranges the 4th and 5th nodes. It cannot swap the data in the nodes, it instead must rearrange pointers.
+
+
+
+#endif //INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
---
+++ LinkedList.h
@@ -0,0 +1,137 @@
+/**
+ * @file: LinkedList.h
+ * @assignment_name: 03 Learning Activity: LinkedList
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 12/13/2022
+ * @brief: Using pointers to create a LinkedList.
+ *
+ */
+
+#ifndef INC_03_LEARING_ACTIVITY_LINKEDLIST_LINKEDLIST_H
+#define INC_03_LEARING_ACTIVITY_LINKEDLIST_LINKEDLIST_H
+
+#include <stdexcept>
+#include <iostream>
+
+
+template<class Type>
+struct Node{
+    Type data;
+    Node<Type> * next;
+};
+
+template<class Type>
+class LinkedList;
+
+template<class Type>
+// Returns an output stream for displaying the LinkedList
+std::ostream& operator<<(std::ostream& out, const LinkedList<Type>& list);
+
+template<class Type>
+class LinkedList{
+public:
+    LinkedList();// Default constructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+    ~LinkedList(); //Destructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+    void insert(Type data); //Adds to the back of the list
+    Type peek(int ndx); //Looks at an element at the given ndx value
+    void remove(int ndx); //Removes an element at the given ndx position
+    friend std::ostream& operator<< <>(std::ostream& out, const LinkedList<Type>& list);// Returns an output stream for displaying the LinkedList
+protected:
+    Node<Type>* front;
+    Node<Type>* back;
+    int count;
+};
+
+template<class Type>
+// Default constructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+LinkedList<Type>::LinkedList(){
+    front = nullptr;
+    back = nullptr;
+    count = 0;
+}
+
+template<class Type>
+//Destructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+LinkedList<Type>::~LinkedList(){
+    Node<Type>* temp = front;
+
+    while(front != nullptr){
+        temp = front;
+        front = front->next;
+        delete temp;
+    }
+}
+
+template<class Type>
+//Adds to the back of the list
+void LinkedList<Type>::insert(Type data){
+    Node<Type>*temp = new Node<Type>();
+    temp->data = data;
+
+    if(!front){
+        this->front = temp;
+        this->back = temp;
+    }
+    else{
+        temp->next = nullptr;
+        back->next = temp;
+        back = temp;
+    }
+    count++;
+}
+
+template<class Type>
+//Looks at an element at the given ndx value
+Type LinkedList<Type>::peek(int ndx){
+    int currentNodeNum = 0;
+    auto currentNode = front; //Only use 'auto' when declaring a pointer and assigning a value for it at the same time
+
+    if(ndx >= count){
+        throw std::runtime_error("Item does not exist");
+    }
+    while(currentNodeNum < ndx){
+        currentNodeNum ++;
+        currentNode = currentNode->next;
+    }
+    return currentNode->data;
+}
+template<class Type>
+//Removes an element at the given ndx position
+void LinkedList<Type>:: remove(int ndx){
+    int currentNodeNum = 0;
+    auto currentNode = front; //Only use 'auto' when declaring a pointer and assigning a value for it at the same time
+
+    if(ndx >= count || ndx < 0){
+        throw std::runtime_error("Item does not exist");
+    }
+
+    if(ndx == 0){
+        front= front->next;
+        delete currentNode;
+        return;
+    }
+
+    while(currentNodeNum < ndx - 1){
+        currentNodeNum ++;
+        currentNode = currentNode->next;
+    }
+    auto toDelete = currentNode->next;
+    currentNode->next = toDelete->next;
+    delete toDelete;
+}
+
+template<class Type>
+// Returns an output stream for displaying the LinkedList
+std::ostream& operator<<(std::ostream& out, const LinkedList<Type>& list){
+    auto currentNode = list.front;
+    while(currentNode){
+        out << currentNode->data;
+        if(currentNode->next){
+            out << " ";
+        }//end if
+        currentNode = currentNode->next;
+    }//end while
+    return out;
+}
+
+#endif //INC_03_LEARING_ACTIVITY_LINKEDLIST_LINKEDLIST_H
