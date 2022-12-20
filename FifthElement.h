9c7331b80abc363a41379497760597be2dd51f80
Wreckeshey <RTSmith801@gmail.com>
2022-12-19 20:36:59 -0700

    code now compiling! Now for the hard part..

--- FifthElement.h
+++ FifthElement.h
@@ -13,17 +13,15 @@
 #include "LinkedList.h"
 
 template<class Type>
-class FifthElement: public LinkedList{
+class FifthElement: public LinkedList<Type>{
 
     private:
 
 
     public:
 
-        template<class Type>
         Type getFifthElement(); //This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
 
-        template<class Type>
         void insertNewFifthElement(const Type &value); //This method inserts a node containing a value between the existing 4 and 5 nodes so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list.
 
         void deleteFifthElement(); //This method deletes the 5th node. If there was a 6th node, the 4th node now points to the 6th node. If there was no 6th node, the 4th node becomes the new back node.
@@ -31,6 +29,28 @@ class FifthElement: public LinkedList{
         void swapFourthAndFifthElement(); //This method rearranges the 4th and 5th nodes. It cannot swap the data in the nodes, it instead must rearrange pointers.
 };
 
+//This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
+template<class Type>
+Type FifthElement<Type>::getFifthElement(){
+    return LinkedList<Type>::peek(4);
+}
+//This method inserts a node containing a value between the existing 4 and 5 nodes so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list.
+template<class Type>
+void FifthElement<Type>::insertNewFifthElement(const Type &value){
+
+}
+
+//This method deletes the 5th node. If there was a 6th node, the 4th node now points to the 6th node. If there was no 6th node, the 4th node becomes the new back node.
+template<class Type>
+void FifthElement<Type>::deleteFifthElement(){
+
+}
+
+//This method rearranges the 4th and 5th nodes. It cannot swap the data in the nodes, it instead must rearrange pointers.
+template<class Type>
+void FifthElement<Type>::swapFourthAndFifthElement(){
+
+}
 
 
 #endif //INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
