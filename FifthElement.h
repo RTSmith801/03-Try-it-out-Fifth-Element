05088ff3c2fe1cded26e36bf45607297d25cd864
Wreckeshey <RTSmith801@gmail.com>
2022-12-19 23:15:04 -0700

    complie working

--- FifthElement.h
+++ FifthElement.h
@@ -3,33 +3,30 @@
  * @assignment_name: 03: Try it out: Fifth Element
  * @author: Rick Smith (rickysmith@mail.weber.edu)
  * @date: 12/13/2022
- * @brief: This program will cover Inheritance and practice handling pointers in Linked Lists.
+ * @brief: This program will cover review Inheritance and give you practice with handling pointers in Linked Lists.
  *
  */
 
-#ifndef FIFTH_ELEMENT_H
-#define FIFTH_ELEMENT_H
+#ifndef INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
+#define INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
+
 #include "LinkedList.h"
 
-<<<<<<< HEAD
 template<class Type>
 class FifthElement: public LinkedList<Type>{
-=======
 
-class FifthElement: public LinkedList<class Type>{
->>>>>>> b7df90d1b006df16904faaff3d238502a4d8dad0
+private:
 
-    private:
 
-    public:
+public:
 
-        Type getFifthElement(); //This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
+    Type getFifthElement(); //This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
 
-        void insertNewFifthElement(const Type &value); //This method inserts a node containing a value between the existing 4 and 5 nodes so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list.
+    void insertNewFifthElement(const Type &value); //This method inserts a node containing a value between the existing 4 and 5 nodes so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list.
 
-        void deleteFifthElement(); //This method deletes the 5th node. If there was a 6th node, the 4th node now points to the 6th node. If there was no 6th node, the 4th node becomes the new back node.
+    void deleteFifthElement(); //This method deletes the 5th node. If there was a 6th node, the 4th node now points to the 6th node. If there was no 6th node, the 4th node becomes the new back node.
 
-        void swapFourthAndFifthElement(); //This method rearranges the 4th and 5th nodes. It cannot swap the data in the nodes, it instead must rearrange pointers.
+    void swapFourthAndFifthElement(); //This method rearranges the 4th and 5th nodes. It cannot swap the data in the nodes, it instead must rearrange pointers.
 };
 
 //This method returns the data at the fifth node (not index) of the linked list. It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
@@ -56,5 +53,4 @@ void FifthElement<Type>::swapFourthAndFifthElement(){
 }
 
 
-
-#endif //FIFTH_ELEMENT__H
+#endif //INC_03_TRY_IT_OUT_FIFTH_ELEMENT_FIFTHELEMENT_H
